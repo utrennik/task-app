5da2fff4e491e2c33c36ba5780ebd458694fdd65
Utrennik <utrennik@list.ru>
2022-03-20 14:15:50 +0300

    fix: fix sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import Header from "./components/header/Header";
 
 function App() {
@@ -31,7 +6,6 @@ function App() {
 			<Header></Header>
 		</div>
 	);
->>>>>>> Initialize project using Create React App
 }
 
 export default App;
